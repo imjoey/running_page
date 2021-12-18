11d1f405f64d363fa6b67adb810f05302bcbbb33
imjoey <majunjie@apache.org>
2021-12-18 21:25:24 +0800

    chore: Personal settings on page
    Signed-off-by: imjoey <majunjie@apache.org>

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,19 +1,11 @@
 module.exports = {
   pathPrefix: `/`, // Change to `/running_page` when running on github pages
   siteMetadata: {
-    siteTitle: 'Running Page',
-    siteUrl: 'https://yihong.run',
-    logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
-    description: 'Personal site and blog',
+    siteTitle: 'Joey Running Page',
+    siteUrl: 'https://running-page-imjoey.vercel.app',
+    logo: 'https://avatars.githubusercontent.com/u/2542401?v=4',
+    description: 'Personal running page',
     navLinks: [
-      {
-        name: 'Blog',
-        url: 'https://yihong.run/running',
-      },
-      {
-        name: 'About',
-        url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
-      },
     ],
   },
   plugins: [
